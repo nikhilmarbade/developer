de8c3a923d42db604a19a609af4abf4dcf4a54fa
nikhilmarbade <nikhilmarbade03@gmail.com>
2023-03-08 12:07:17 +0530

    Latest Cpmmit

---
+++ test_module.py
@@ -0,0 +1,11 @@
+print('This is a Test Module.....')
+
+# import multiplication
+import addition
+import config
+# addition.get_addition(12,56)
+# multiplication.get_multiplication(12,6)
+print('Value in __Name__ variable is :', __name__)
+
+print(config.IMAGE_PATH)
+print(config.MONGODB_PORT_NUMBER)
